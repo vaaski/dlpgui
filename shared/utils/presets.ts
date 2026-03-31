9519f7dbdf5f0a7a0e3a46f47980fda3db833b07
vaaski <admin@vaa.ski>
2026-03-31 21:41:03 +0200

    explicitly no playlists

--- shared/utils/presets.ts
+++ shared/utils/presets.ts
@@ -1,5 +1,7 @@
+export const sharedFlags = ["--no-playlist"]
+
 export const presets = {
-	mp3: ["-x", "--audio-format", "mp3"],
-	mp4: ["-f", "b", "--remux-video", "mp4"],
-	mp4_best: ["-f", "bv*+ba/b", "--remux-video", "mp4"],
+	mp3: [...sharedFlags, "-x", "--audio-format", "mp3"],
+	mp4: [...sharedFlags, "-f", "b", "--remux-video", "mp4"],
+	mp4_best: [...sharedFlags, "-f", "bv*+ba/b", "--remux-video", "mp4"],
 } as const
